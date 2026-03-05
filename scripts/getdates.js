27b6c6698a71e412de8469843b83fe957e5b4ad8
Chrisman1-BYU <casamoah@byupathway.edu>
2026-03-05 15:09:14 +0000

    Main

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,6 +1,4 @@
-// Get current year
 const year = new Date().getFullYear();
 document.getElementById("currentyear").textContent = year;
 
-// Get last modified date 
-document.getElementById("lastModified").textContent = document.lastModified;
+document.getElementById("lastModified").textContent = "Last Modified: " + document.lastModified;
