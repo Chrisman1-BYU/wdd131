9af43bb27b6e740f46919fdc203b359761f83998
Chrisman1-BYU <casamoah@byupathway.edu>
2026-03-05 15:18:34 +0000

    Update homepage project

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,4 +1,4 @@
 const year = new Date().getFullYear();
 document.getElementById("currentyear").textContent = year;
 
-document.getElementById("lastModified").textContent = "Last Modified: " + document.lastModified;
+document.getElementById("lastModified").textContent = "Last Modified: " + document.lastModified; 
