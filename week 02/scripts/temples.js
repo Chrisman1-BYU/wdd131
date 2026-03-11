10ccb9a22160aed1fd36728af23ac25ab4ac1e8b
Chrisman1-BYU <casamoah@byupathway.edu>
2026-03-11 14:51:33 +0000

    Temple Update

---
+++ week 02/scripts/temples.js
@@ -0,0 +1,21 @@
+const button = document.querySelector("#menu-button");
+const menu = document.querySelector("#nav-menu");
+
+button.addEventListener("click", () => {
+menu.classList.toggle("open");
+
+if(button.textContent === "☰"){
+button.textContent = "X";
+} else {
+button.textContent = "☰";
+}
+
+});
+
+/* footer dates */
+
+document.querySelector("#year").textContent =
+new Date().getFullYear();
+
+document.querySelector("#lastModified").textContent =
+"Last Modified: " + document.lastModified;
