4e7c2cd99c2e7a789d213016535aa89a0b5174b5
Chrisman1-BYU <casamoah@byupathway.edu>
2026-03-24 14:32:04 +0000

    Updated Country Page

--- scripts/place.js
+++ scripts/place.js
@@ -6,8 +6,8 @@ year.textContent = new Date().getFullYear();
 lastModified.textContent = document.lastModified;
 
 // WEATHER VALUES (STATIC)
-const temp = 8;
-const windSpeed = 10;
+const temp = 8; // °C
+const windSpeed = 10; // km/h
 
 function calculateWindChill(t, v) {
     return 13.12 + (0.6215 * t) - (11.37 * Math.pow(v, 0.16)) + (0.3965 * t * Math.pow(v, 0.16));
@@ -19,4 +19,4 @@ if (temp <= 10 && windSpeed > 4.8) {
     windChill = calculateWindChill(temp, windSpeed).toFixed(2) + "°C";
 }
 
-document.getElementById("windChill").textContent = windChill;
+document.getElementById("windChill").textContent = windChill; 
