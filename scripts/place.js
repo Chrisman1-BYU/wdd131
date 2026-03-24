ebd530c3ee673592f4f036bfbe8375de89a7a42b
Chrisman1-BYU <casamoah@byupathway.edu>
2026-03-24 14:38:54 +0000

    added svg weather icom amd improved meta description

--- scripts/place.js
+++ scripts/place.js
@@ -7,7 +7,7 @@ lastModified.textContent = document.lastModified;
 
 // WEATHER VALUES (STATIC)
 const temp = 8; // °C
-const windSpeed = 10; // km/h
+const windSpeed = 10; // km/h 
 
 function calculateWindChill(t, v) {
     return 13.12 + (0.6215 * t) - (11.37 * Math.pow(v, 0.16)) + (0.3965 * t * Math.pow(v, 0.16));
