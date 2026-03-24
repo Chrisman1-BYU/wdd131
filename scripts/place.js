6dd05493109d44245f49af0b19f3a150da1cc1c6
Chrisman1-BYU <casamoah@byupathway.edu>
2026-03-24 13:26:56 +0000

    Country Page assignment

---
+++ scripts/place.js
@@ -0,0 +1,22 @@
+// Footer Dates
+const year = document.getElementById("year");
+const lastModified = document.getElementById("lastModified");
+
+year.textContent = new Date().getFullYear();
+lastModified.textContent = document.lastModified;
+
+// WEATHER VALUES (STATIC)
+const temp = 8;
+const windSpeed = 10;
+
+function calculateWindChill(t, v) {
+    return 13.12 + (0.6215 * t) - (11.37 * Math.pow(v, 0.16)) + (0.3965 * t * Math.pow(v, 0.16));
+}
+
+let windChill = "N/A";
+
+if (temp <= 10 && windSpeed > 4.8) {
+    windChill = calculateWindChill(temp, windSpeed).toFixed(2) + "°C";
+}
+
+document.getElementById("windChill").textContent = windChill;
